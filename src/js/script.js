7d6f18e554d180af618e8f92d4b464a52f94c074
irapets777 <irapets777@gmail.com>
2023-01-21 20:52:39 +0200

    start

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,9 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+/*Task 1*/
+
+let userName = prompt('enter your name:');
+alert(`Hello, ${userName}!`)
+
+/*Task 2*/
+
+const year = 2023;
 
